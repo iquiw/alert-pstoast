e9ffa333ca052ae4cb02f9b0d4deb67196da0dba
Iku Iwasa <iku.iwasa@gmail.com>
2019-05-24 00:09:33 +0900

    Add option to specify App name in toast.ps1

--- toast.ps1
+++ toast.ps1
@@ -1,3 +1,7 @@
+param(
+    [String] $Name
+)
+
 [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom, ContentType = WindowsRuntime] > $null
 [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
 
@@ -6,7 +10,12 @@ $xml.LoadXml([Console]::In.ReadToEnd())
 
 $toast = [Windows.UI.Notifications.ToastNotification]::new($xml)
 
-$app = Get-StartApps -Name "Windows Powershell" | Select-Object -Index 0
+if ($Name.Length -gt 0) {
+    $app = Get-StartApps -Name $Name | Select-Object -Index 0
+}
+if ($app -eq $null) {
+    $app = Get-StartApps -Name "Windows Powershell" | Select-Object -Index 0
+}
 
 $notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($app.AppID)
 $notifier.Show($toast);
